26176889484bf1742f07f6deb58b1c29130298ae
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <riguetemartins@gmail.com>
2024-11-07 15:42:21 -0300

    atualizada

--- distancia_entre_dois_plano_cartesiano.c
+++ distancia_entre_dois_plano_cartesiano.c
@@ -1,9 +1,35 @@
-#include <stdlib.hd>
+#include <stdlib.h>
 #include <stdio.h>
 
+struct plano2d{
+	int x;
+	int y;
+};
+typedef struct plano2d plano2d;
 
-int main(int argc, char const *argv[])
-{
-	
+void imprimir ();
+void opcao_verif_process(int*);
+
+int main(int argc, char const *argv[]){
+	int opcao;
+
+		do{
+			imprimir();
+			opcao_verif_process(&opcao);
+		}while(opcao != 0);
+
 	return 0;
+}
+
+void imprimir (){
+	puts("0-fechar programa");
+	puts("1-criar_ponto");
+	puts("2-listar");
+	puts("3-alterar");
+	puts("4-exluir_ponto");
+};
+
+void opcao_verif_process(int *opcao){
+	scanf("%i", *opcao);
+	printf("%i\n", *opcao);
 }
