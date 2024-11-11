4f136463937935cb22744968b73dc1f65d639e7f
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <riguetepedro0@gmail.com>
2024-11-11 11:32:45 -0300

    atualizado

--- distancia_entre_dois_plano_cartesiano.c
+++ distancia_entre_dois_plano_cartesiano.c
@@ -1,35 +1,78 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-struct plano2d{
+struct pontos2d
+{
 	int x;
 	int y;
 };
-typedef struct plano2d plano2d;
 
-void imprimir ();
-void opcao_verif_process(int*);
+typedef struct pontos2d t_ponto2d;
 
-int main(int argc, char const *argv[]){
-	int opcao;
+struct controle_pontos2D
+{
+	t_pontos2d* pontos2d;
+	int quantidade;
+};
 
-		do{
-			imprimir();
-			opcao_verif_process(&opcao);
-		}while(opcao != 0);
+typedef struct controle_pontos2D t_controle_pontos;
 
+void imprimir();
+void obter_opcao(int*);
+void processar_e_direcionar_funcionalidade_opcao(t_plano_2D*,int);
+
+int main(int argc, char** argv[])
+{
+	t_controle_pontos controle_pontos2D;
+	int *opcao;
+	do{
+		imprimir();
+		obter_opcao(&opcao);
+		processar_e_direcionar_funcionalidade_opcao(t_controle_pontos,opcao);
+	}while(opcao != 0);
 	return 0;
 }
 
-void imprimir (){
-	puts("0-fechar programa");
-	puts("1-criar_ponto");
-	puts("2-listar");
-	puts("3-alterar");
-	puts("4-exluir_ponto");
-};
-
-void opcao_verif_process(int *opcao){
-	scanf("%i", *opcao);
-	printf("%i\n", *opcao);
+void imprimir(){
+	puts("0 - sair do programa ");
+	puts("1 - cadastrar ponto 2D");
+	puts("2 - listar pontos 2D");
+	puts("3 - excluir ponto 2D");
+	puts("4 - alterar ponto 2D");
+	puts("digite sua opcao:");
 }
+
+void obter_opcao(int *opcao){
+scanf("%i", opcao);
+
+}
+
+void processar_e_direcionar_funcionalidade_opcao(t_controle_pontos controle_pontos2D,int opcao){
+   switch(opcao){
+    case 0 :
+      printf("saindo do programa");
+      break;
+    case 1 : 
+      cadastrar();
+      break;
+    case 2 :
+      listar_opcao();
+      break;
+    case 3 :
+      excluir_opcao();
+      break;
+    case 4 :
+      alterar_opcao();
+      break;
+    default:
+      printf("voce digitou uma opcao invalida\n");
+  };
+}
+
+cadastrar();
+
+listar_opcao();
+
+excluir_opcao();
+
+alterar_opcao();
